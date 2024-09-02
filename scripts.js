d77d4669bb59a9f110e4c15419d86a6bd1310fd6
zippy0626 <zippy0626@gmail.com>
2024-09-02 11:28:46 -0400

    Added main playRound logic and resetScores

--- scripts.js
+++ scripts.js
@@ -11,6 +11,7 @@ function getComputerChoice() {
     }
 }
 
+
 // Prompt Human choice
 function getHumanChoice() {
     let choice = prompt("What is your choice? (Rock, Paper, or Scissors)?")
@@ -19,3 +20,53 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+
+function playRound() {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+
+    
+    if ((humanChoice === 'rock' || humanChoice === 'r') && computerChoice === 'Scissors') {
+        humanScore += 1
+        console.log('You win, boy. Rock beats scissors, duh.');
+        console.log(`Scores: You (unimpressive) ${humanScore}  Computer ${computerScore}`);
+        console.log('\n');
+
+    } else if ((humanChoice === 'scissors' || humanChoice === 's') && computerChoice === 'Paper') {
+        humanScore += 1
+        console.log('You win... unimpressive. Scissors beat paper genius.');
+        console.log(`Scores: You (ew) ${humanScore}  Computer ${computerScore}`);
+        console.log('\n');
+
+    } else if ((humanChoice === 'paper' || humanChoice === 'p') && computerChoice === 'Rock') {
+        humanScore += 1
+        console.log('You win, but you are still a loser.. Paper beats rock!');
+        console.log(`Scores: You (barf) ${humanScore}  Computer ${computerScore}`);
+        console.log('\n');
+
+    } else if (humanChoice === computerChoice.toLowerCase() || humanChoice === computerChoice[0].toLowerCase()) {
+        console.log(`Tie, for now.. ${humanChoice} clashes with ${computerChoice.toLowerCase()}!!!`);
+        console.log(`Scores: You (me?) ${humanScore}  Computer ${computerScore}`);
+        console.log('\n');
+    
+    } else {
+
+        if (humanScore > 0) {
+            humanScore -= 1
+        }
+
+        computerScore += 1
+        console.log(`You suck! Haha, I knew you were a loser. ${computerChoice} beats ${humanChoice}!`);
+        console.log(`Scores: You (ICU) ${humanScore}  Computer ${computerScore}`);
+        console.log('\n');
+    };
+}
+
+
+function resetScores() {
+    humanScore = 0;
+    computerScore = 0;
+}
+
+
